081028aa45b9efd6a2dd847b53e3e02d7fcc301b
CrisisAlpha <tktaiaa@connect.ust.hk>
2025-04-19 02:45:33 +0800

    Update tile layer URL to use OpenStreetMap tiles

--- src/App.js
+++ src/App.js
@@ -867,7 +867,7 @@ function App() {
                 style={{ height: '500px', width: '100%' }}
               >
                 <TileLayer
-                  url="/tiles/{z}/{x}/{y}.png"
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 />
                 <HeatmapLayer data={pins} show={showHeatmap} />
